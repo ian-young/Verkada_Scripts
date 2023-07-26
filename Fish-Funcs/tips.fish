5c69885d343e3e95eb0a8bf691a42fc3d52127b6
Ian Young <ian.young@Ian-Young-FXW43YWW6J.local>
2023-07-26 15:08:55 -0600

    Increased tip clarity and removed unnecessary commands.

--- Fish-Funcs/tips.fish
+++ Fish-Funcs/tips.fish
@@ -22,9 +22,8 @@ function tips --description 'Prints tips for vToolbox'
 	echo ""
 	echo "$(set_color normal)vtoolbox camera.set-framerate -a $(set_color cyan)<token>$(set_color normal) -f $(set_color cyan)<new frame rate Integer>$(set_color normal) -s $(set_color cyan)<serial>$(set_color yellow) Change a camera's framerate.$(set_color normal)"
 	echo "vtoolbox device.set-device-config -d $(set_color cyan)<deviceID> $(set_color normal)-u $(set_color cyan)<auth Token>$(set_color normal) -p \"image-processing.night-max-shutter-time-hyperzoom-hz\" -a $(set_color cyan)<value> $(set_color yellow) LPR shutter speed.$(set_color normal)"
-	echo "vtoolbox device.set-device-config -u $(set_color cyan)<TOKEN> $(set_color normal)-d $(set_color cyan)<camID> $(set_color normal)-p 'camera-config.ldc-enable' 'true' $(set_color yellow)Enable LDC (Reboot after this)$(set_color normal)"
-	echo "vtoolbox device.set-device-config -u $(set_color cyan)<TOKEN> $(set_color normal)-d $(set_color cyan)<camID> $(set_color normal)-p 'camera-config.ldc-enable' 'false' $(set_color yellow)Disable LDC to increase FOV.$(set_color normal)"
-
-
+	echo "--------------------------------------------------------"
+	echo ""
+	echo "When running a fish function for vToolbox, \$auth <support token>, \$org <org ID>, \$serial <serial number>, and \$device <device ID> will need to be set."
         echo "--------------------------------------------------------"
 end
