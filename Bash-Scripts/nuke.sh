262eac1219295029ca4f7c5d3d05ecb8332936af
Ian-Young <ianyoung0107@gmail.com>
2023-08-03 11:53:35 -0600

    Added multi-threading.

--- Bash-Scripts/nuke.sh
+++ Bash-Scripts/nuke.sh
@@ -65,88 +65,91 @@ nukeOrg () {
 
 			# Start camera config (snapshot)
 			walker=$(cat cameras.setting | grep http-proxy | awk -F ':' '{print $4}')
-			vtoolbox set-device-config -u $auth -d $device -p 'http-proxy.backup-aws' "https:$walker"
+			vtoolbox set-device-config -u $auth -d $device -p 'http-proxy.backup-aws' "https:$walker" &
 			walker=$(cat cameras.setting | grep backup-bb | awk -F ':' '{print $3}')
-			vtoolbox set-device-config -u $auth -d $device -p 'backup-bb' "https:$walker"
+			vtoolbox set-device-config -u $auth -d $device -p 'backup-bb' "https:$walker" &
 			walker=$(cat cameras.setting | grep loggly | awk -F ':' '{print $3}')
-			vtoolbox set-device-config -u $auth -d $device -p 'loggly' "https:$walker"
+			vtoolbox set-device-config -u $auth -d $device -p 'loggly' "https:$walker" &
 			walker=$(cat cameras.setting | grep vauth | awk -F ':' '{print $3}')
-			vtoolbox set-device-config -u $auth -d $device -p 'vauth' "https:$walker"
+			vtoolbox set-device-config -u $auth -d $device -p 'vauth' "https:$walker" &
 			walker=$(cat cameras.setting | grep vcameramgmt | awk -F ':' '{print $3}')
-			vtoolbox set-device-config -u $auth -d $device -p 'vcameramgmt' "https:$walker"
+			vtoolbox set-device-config -u $auth -d $device -p 'vcameramgmt' "https:$walker" &
 			walker=$(cat cameras.setting | grep vflow | awk -F ':' '{print $3}')
-			vtoolbox set-device-config -u $auth -d $device -p 'vflow' "wss:$walker"
+			vtoolbox set-device-config -u $auth -d $device -p 'vflow' "wss:$walker" &
 			walker=$(cat cameras.setting | grep vhistory | awk -F ':' '{print $3}')
-			vtoolbox set-device-config -u $auth -d $device -p 'vhistory' "wss:$walker"
+			vtoolbox set-device-config -u $auth -d $device -p 'vhistory' "wss:$walker" &
 			walker=$(cat cameras.setting | grep vosprey | awk -F ':' '{print $3}')
-			vtoolbox set-device-config -u $auth -d $device -p 'vosprey' "https:$walker"
+			vtoolbox set-device-config -u $auth -d $device -p 'vosprey' "https:$walker" &
 			walker=$(cat cameras.setting | grep vprovision | awk -F ':' '{print $3}')
-			vtoolbox set-device-config -u $auth -d $device -p 'vprovision' "https:$walker"
+			vtoolbox set-device-config -u $auth -d $device -p 'vprovision' "https:$walker" &
 			walker=$(cat cameras.setting | grep vproxy | awk -F ':' '{print $3}' | head -1)
-			vtoolbox set-device-config -u $auth -d $device -p 'vproxy' "https:$walker"
+			vtoolbox set-device-config -u $auth -d $device -p 'vproxy' "https:$walker" &
 			walker=$(cat cameras.setting | grep vstream | awk -F ':' '{print $3}' | head -1)
-			vtoolbox set-device-config -u $auth -d $device -p 'vstream' "wss:$walker"
+			vtoolbox set-device-config -u $auth -d $device -p 'vstream' "wss:$walker" &
 			walker=$(cat cameras.setting | grep vsubmit | awk -F ':' '{print $3}')
-			vtoolbox set-device-config -u $auth -d $device -p 'vsubmit' "https:$walker"
+			vtoolbox set-device-config -u $auth -d $device -p 'vsubmit' "https:$walker" &
 			walker=$(cat cameras.setting | grep vsubmit | awk -F ':' '{print $3}')
-			vtoolbox set-device-config -u $auth -d $device -p 'motion-threshold' "$walker"
+			vtoolbox set-device-config -u $auth -d $device -p 'motion-threshold' "$walker" &
 			walker=$(cat cameras.setting | grep object-tracking-hz-interval-high-quality | awk -F ':' '{print $2}')
-			vtoolbox set-device-config -u $auth -d $device -p 'object-tracking-hz-interval-high-quality' "$walker"
+			vtoolbox set-device-config -u $auth -d $device -p 'object-tracking-hz-interval-high-quality' "$walker" &
 			walker=$(cat cameras.setting | grep object-tracking-hz-interval-low-quality | awk -F ':' '{print $2}')
-			vtoolbox set-device-config -u $auth -d $device -p 'object-tracking-hz-interval-low-quality' "$walker"
+			vtoolbox set-device-config -u $auth -d $device -p 'object-tracking-hz-interval-low-quality' "$walker" &
 			walker=$(cat cameras.setting | grep object-tracking-max-lost-tracklet-age | awk -F ':' '{print $2}')
-			vtoolbox set-device-config -u $auth -d $device -p 'object-tracking-max-lost-tracklet-age' "$walker"
+			vtoolbox set-device-config -u $auth -d $device -p 'object-tracking-max-lost-tracklet-age' "$walker" &
 			walker=$(cat cameras.setting | grep object-tracking-movement-threshold-low-quality| awk -F ':' '{print $2}')
-			vtoolbox set-device-config -u $auth -d $device -p 'object-tracking-movement-threshold-low-quality' "$walker"
+			vtoolbox set-device-config -u $auth -d $device -p 'object-tracking-movement-threshold-low-quality' "$walker" &
 			walker=$(cat cameras.setting | grep person-crop-threshold | awk -F ':' '{print $2}')
-			vtoolbox set-device-config -u $auth -d $device -p 'person-crop-threshold' "$walker"
+			vtoolbox set-device-config -u $auth -d $device -p 'person-crop-threshold' "$walker" &
 			walker=$(cat cameras.setting | grep person-intrusion-threshold | awk -F ':' '{print $2}')
-			vtoolbox set-device-config -u $auth -d $device -p 'person-intrusion-threshold' "$walker"
+			vtoolbox set-device-config -u $auth -d $device -p 'person-intrusion-threshold' "$walker" &
 			walker=$(cat cameras.setting | grep person-send-full-frame-threshold | awk -F ':' '{print $2}')
-			vtoolbox set-device-config -u $auth -d $device -p 'person-send-full-frame-threshold' "$walker"
+			vtoolbox set-device-config -u $auth -d $device -p 'person-send-full-frame-threshold' "$walker" &
 			walker=$(cat cameras.setting | grep vehicle-intrusion-threshold | awk -F ':' '{print $2}')
-			vtoolbox set-device-config -u $auth -d $device -p 'vehicle-intrusion-threshold' "$walker"
+			vtoolbox set-device-config -u $auth -d $device -p 'vehicle-intrusion-threshold' "$walker" &
 			walker=$(cat cameras.setting | grep vehicle-send-full-frame-threshold | awk -F ':' '{print $2}')
-			vtoolbox set-device-config -u $auth -d $device -p 'vehicle-send-full-frame-threshold' "$walker"
+			vtoolbox set-device-config -u $auth -d $device -p 'vehicle-send-full-frame-threshold' "$walker" &
 			walker=$(cat cameras.setting | grep vprox-url | awk -F ':' '{print $2}')
-			vtoolbox set-device-config -u $auth -d $device -p 'vproxy-url' "$walker"
+			vtoolbox set-device-config -u $auth -d $device -p 'vproxy-url' "$walker" &
 			walker=$(cat cameras.setting | grep image-night-ae-compensation | awk -F ':' '{print $3}')
-			vtoolbox set-device-config -u $auth -d $device -p 'image-processing.image-night-ae-compensation' "$walker"
+			vtoolbox set-device-config -u $auth -d $device -p 'image-processing.image-night-ae-compensation' "$walker" &
 			walker=$(cat cameras.setting | grep low-lux-mode-off-thresh | awk -F ':' '{print $2}')
-			vtoolbox set-device-config -u $auth -d $device -p 'low-lux-mode-off-thresh' "$walker"
+			vtoolbox set-device-config -u $auth -d $device -p 'low-lux-mode-off-thresh' "$walker" &
 			walker=$(cat cameras.setting | grep max-shutter-time-hyperzoom-hz | awk -F ':' '{print $2}' | head -1)
-			vtoolbox set-device-config -u $auth -d $device -p 'max-shutter-time-hyperzoom-hz' "$walker"
+			vtoolbox set-device-config -u $auth -d $device -p 'max-shutter-time-hyperzoom-hz' "$walker" &
 			walker=$(cat cameras.setting | grep max-shutter-time-hyperzoom-hz | awk -F ':' '{print $2}' | tail -1)
-			vtoolbox set-device-config -u $auth -d $device -p 'night-max-shutter-time-hyperzoom-hz' "$walker"
+			vtoolbox set-device-config -u $auth -d $device -p 'night-max-shutter-time-hyperzoom-hz' "$walker" &
 			walker=$(cat cameras.setting | grep detection-person-confidence-threshold | awk -F ':' '{print $2}')
 			vtoolbox set-device-config -u $auth -d $device -p \
-				'motion-search.detection-person-confidence-threshold' "$walker"
+				'motion-search.detection-person-confidence-threshold' "$walker" &
 			walker=$(cat cameras.setting | grep detection-vehicle-confidence-threshold | awk -F ':' '{print $2}')
-			vtoolbox set-device-config -u $auth -d $device -p 'detection-vehicle-confidence-threshold' "$walker"
+			vtoolbox set-device-config -u $auth -d $device -p 'detection-vehicle-confidence-threshold' "$walker" &
 			walker=$(cat cameras.setting | grep camera-rotate | awk -F ':' '{print $2}')
-			vtoolbox set-device-config -u $auth -d $device -p 'camera-rotate' "$walker"
+			vtoolbox set-device-config -u $auth -d $device -p 'camera-rotate' "$walker" &
 			walker=$(cat cameras.setting | grep max-viewers | awk -F ':' '{print $3}')
-			vtoolbox set-device-config -u $auth -d $device -p 'rtsp.max-viewers' "$walker"
+			vtoolbox set-device-config -u $auth -d $device -p 'rtsp.max-viewers' "$walker" &
 			walker=$(cat cameras.setting | grep channel | awk -F ':' '{print $3}')
-			vtoolbox set-device-config -u $auth -d $device -p 'release-info.channel' "$walker"
+			vtoolbox set-device-config -u $auth -d $device -p 'release-info.channel' "$walker" &
 			walker=$(cat cameras.setting | grep system-stats-window | awk -F ':' '{print $2}')
-			vtoolbox set-device-config -u $auth -d $device -p 'stats-monitor-config.system-stats-window' "$walker"
+			vtoolbox set-device-config -u $auth -d $device -p 'stats-monitor-config.system-stats-window' "$walker" &
 			walker=$(cat cameras.setting | grep cloud-backup-location | awk -F ':' '{print $2}')
-			vtoolbox set-device-config -u $auth -d $device -p 'storage-manage.cloud-backup-location' "$walker"
+			vtoolbox set-device-config -u $auth -d $device -p 'storage-manage.cloud-backup-location' "$walker" &
 			walker=$(cat cameras.setting | grep stream-encoding | awk -F ':' '{print $4}' | head -1)
-			vtoolbox set-device-config -u $auth -d $device -p 'stream-configs.high.stream-encoding' "$walker"
+			vtoolbox set-device-config -u $auth -d $device -p 'stream-configs.high.stream-encoding' "$walker" &
 			walker=$(cat cameras.setting | grep stream-encoding | awk -F ':' '{print $3}' | tail -1)
-			vtoolbox set-device-config -u $auth -d $device -p 'low.stream-encoding' "$walker"
+			vtoolbox set-device-config -u $auth -d $device -p 'low.stream-encoding' "$walker" &
 			walker=$(cat cameras.setting | grep cloud-backup-presigned-timeout | awk -F ':' '{print $2}')
-			vtoolbox set-device-config -u $auth -d $device -p 'cloud-backup-presigned-timeout' "$walker"
+			vtoolbox set-device-config -u $auth -d $device -p 'cloud-backup-presigned-timeout' "$walker" &
 			walker=$(cat cameras.setting | grep record-on-motion-expansion | awk -F ':' '{print $2}')
-			vtoolbox set-device-config -u $auth -d $device -p 'record-on-motion-expansion' "$walker"
+			vtoolbox set-device-config -u $auth -d $device -p 'record-on-motion-expansion' "$walker" &
 			walker=$(cat cameras.setting | grep thumbnail-repeat-interval | awk -F ':' '{print $2}')
-			vtoolbox set-device-config -u $auth -d $device -p 'thumbnail-repeat-interval' "$walker"
+			vtoolbox set-device-config -u $auth -d $device -p 'thumbnail-repeat-interval' "$walker" &
 			walker=$(cat cameras.setting | grep motion-notification-divisor | awk -F ':' '{print $3}')
-			vtoolbox set-device-config -u $auth -d $device -p 'vda.motion-notification-divisor' "$walker"
+			vtoolbox set-device-config -u $auth -d $device -p 'vda.motion-notification-divisor' "$walker" &
 			walker=$(cat cameras.setting | grep | awk -F ':' '{print $4}' | tail -1)
-			vtoolbox set-device-config -u $auth -d $device -p 'vstream.high.enabled' "$walker"
+			vtoolbox set-device-config -u $auth -d $device -p 'vstream.high.enabled' "$walker" &
+			# The assumption that the last thread will have the same runtime as the previous threads
+			LT=${!} # Get the pid of the last thread
+			wait ${LT} # Wait for the thread to finish
 			echo "End of camera reset for $device."	
 		done
 		# End of camera config (snapshot)
@@ -163,49 +166,52 @@ nukeOrg () {
 
 			# Start camera config (dome default)
 			vtoolbox set-device-config -u $auth -d $device -p \
-				'http-proxy.backup-aws' 'https://s3.us-west-2.amazonaws.com'
-			vtoolbox set-device-config -u $auth -d $device -p 'backup-bb' 'https://s3.us-west-004.backblazeb2.com'
-			vtoolbox set-device-config -u $auth -d $device -p 'loggly' 'https://api.control.verkada.com/logs/relay'
-			vtoolbox set-device-config -u $auth -d $device -p 'vauth' 'https://api.control.verkada.com/device/auth'
-			vtoolbox set-device-config -u $auth -d $device -p 'vcameramgmt' 'https://api.control.verkada.com/manage'
+				'http-proxy.backup-aws' 'https://s3.us-west-2.amazonaws.com' &
+			vtoolbox set-device-config -u $auth -d $device -p 'backup-bb' 'https://s3.us-west-004.backblazeb2.com' &
+			vtoolbox set-device-config -u $auth -d $device -p 'loggly' 'https://api.control.verkada.com/logs/relay' &
+			vtoolbox set-device-config -u $auth -d $device -p 'vauth' 'https://api.control.verkada.com/device/auth' &
+			vtoolbox set-device-config -u $auth -d $device -p 'vcameramgmt' 'https://api.control.verkada.com/manage' &
 			vtoolbox set-device-config -u $auth -d $device -p 'vflow' 'wss://relay.control.verkada.com/flow/'
 			vtoolbox set-device-config -u $auth -d $device -p \
-				'vhistory' 'wss://relay.control.verkada.com/vhistory/camera'
-			vtoolbox set-device-config -u $auth -d $device -p 'vospery' 'https://relay.control.verkada.com/vosprey'
-			vtoolbox set-device-config -u $auth -d $device -p 'vprovision' 'https://api.control.verkada.com'
-			vtoolbox set-device-config -u $auth -d $device -p 'vproxy' 'https://relay.control.verkada.com'
-			vtoolbox set-device-config -u $auth -d $device -p 'vstream' 'wss://relay.control.verkada.com/stream'
-			vtoolbox set-device-config -u $auth -d $device -p 'vsubmit' 'https://index.control.verkada.com'
-			vtoolbox set-device-config -u $auth -d $device -p 'motion-threshold' '120'
-			vtoolbox set-device-config -u $auth -d $device -p 'object-tracking-hz-interval-high-quality' '8000'
-			vtoolbox set-device-config -u $auth -d $device -p 'object-tracking-hz-interval-low-quality' '60000'
-			vtoolbox set-device-config -u $auth -d $device -p 'object-tracking-max-lost-tracklet-age' '20'
-			vtoolbox set-device-config -u $auth -d $device -p 'object-tracking-movement-threshold-low-quality' '0.899'
-			vtoolbox set-device-config -u $auth -d $device -p 'person-crop-threshold' '0.83'
-			vtoolbox set-device-config -u $auth -d $device -p 'person-intrusion-threshold' '0.5'
-			vtoolbox set-device-config -u $auth -d $device -p 'person-send-full-frame-threshold' '0.8'
-			vtoolbox set-device-config -u $auth -d $device -p 'vehicle-intrusion-threshold' '0.5'
-			vtoolbox set-device-config -u $auth -d $device -p 'vehicle-send-full-frame-threshold' '0.75'
-			vtoolbox set-device-config -u $auth -d $device -p 'vproxy-url' 'relay.control.verkada.com'
-			vtoolbox set-device-config -u $auth -d $device -p 'image-processing.image-night-ae-compensation' '100'
-			vtoolbox set-device-config -u $auth -d $device -p 'low-lux-mode-off-thresh' '8'
-			vtoolbox set-device-config -u $auth -d $device -p 'max-shutter-time-hyperzoom-hz' '60'
-			vtoolbox set-device-config -u $auth -d $device -p 'night-max-shutter-time-hyperzoom-hz' '60'
+				'vhistory' 'wss://relay.control.verkada.com/vhistory/camera' &
+			vtoolbox set-device-config -u $auth -d $device -p 'vospery' 'https://relay.control.verkada.com/vosprey' &
+			vtoolbox set-device-config -u $auth -d $device -p 'vprovision' 'https://api.control.verkada.com' &
+			vtoolbox set-device-config -u $auth -d $device -p 'vproxy' 'https://relay.control.verkada.com' &
+			vtoolbox set-device-config -u $auth -d $device -p 'vstream' 'wss://relay.control.verkada.com/stream' &
+			vtoolbox set-device-config -u $auth -d $device -p 'vsubmit' 'https://index.control.verkada.com' &
+			vtoolbox set-device-config -u $auth -d $device -p 'motion-threshold' '120' &
+			vtoolbox set-device-config -u $auth -d $device -p 'object-tracking-hz-interval-high-quality' '8000' &
+			vtoolbox set-device-config -u $auth -d $device -p 'object-tracking-hz-interval-low-quality' '60000' &
+			vtoolbox set-device-config -u $auth -d $device -p 'object-tracking-max-lost-tracklet-age' '20' &
+			vtoolbox set-device-config -u $auth -d $device -p 'object-tracking-movement-threshold-low-quality' '0.899' &
+			vtoolbox set-device-config -u $auth -d $device -p 'person-crop-threshold' '0.83' &
+			vtoolbox set-device-config -u $auth -d $device -p 'person-intrusion-threshold' '0.5' &
+			vtoolbox set-device-config -u $auth -d $device -p 'person-send-full-frame-threshold' '0.8' &
+			vtoolbox set-device-config -u $auth -d $device -p 'vehicle-intrusion-threshold' '0.5' &
+			vtoolbox set-device-config -u $auth -d $device -p 'vehicle-send-full-frame-threshold' '0.75' &
+			vtoolbox set-device-config -u $auth -d $device -p 'vproxy-url' 'relay.control.verkada.com' &
+			vtoolbox set-device-config -u $auth -d $device -p 'image-processing.image-night-ae-compensation' '100' &
+			vtoolbox set-device-config -u $auth -d $device -p 'low-lux-mode-off-thresh' '8' &
+			vtoolbox set-device-config -u $auth -d $device -p 'max-shutter-time-hyperzoom-hz' '60' &
+			vtoolbox set-device-config -u $auth -d $device -p 'night-max-shutter-time-hyperzoom-hz' '60' &
 			vtoolbox set-device-config -u $auth -d $device -p \
-				'motion-search.detection-person-confidence-threshold' '0.68'
-			vtoolbox set-device-config -u $auth -d $device -p 'detection-vehicle-confidence-threshold' '0.6'
-			vtoolbox set-device-config -u $auth -d $device -p 'camera-rotate' '180'
-			vtoolbox set-device-config -u $auth -d $device -p 'rtsp.max-viewers' '2'
-			vtoolbox set-device-config -u $auth -d $device -p 'release-info.channel' 'customer-stable'
-			vtoolbox set-device-config -u $auth -d $device -p 'stats-monitor-config.system-stats-window' '180'
-			vtoolbox set-device-config -u $auth -d $device -p 'storage-manage.cloud-backup-location' 'b2'
-			vtoolbox set-device-config -u $auth -d $device -p 'stream-configs.high.stream-encoding' 'HEVC'
-			vtoolbox set-device-config -u $auth -d $device -p 'low.stream-encoding' 'HEVC'
-			vtoolbox set-device-config -u $auth -d $device -p 'cloud-backup-presigned-timeout' '60'
-			vtoolbox set-device-config -u $auth -d $device -p 'record-on-motion-expansion' '5'
-			vtoolbox set-device-config -u $auth -d $device -p 'thumbnail-repeat-interval' '300'
-			vtoolbox set-device-config -u $auth -d $device -p 'vda.motion-notification-divisor' '4'
-			vtoolbox set-device-config -u $auth -d $device -p 'vstream.high.enabled' 'false'
+				'motion-search.detection-person-confidence-threshold' '0.68' &
+			vtoolbox set-device-config -u $auth -d $device -p 'detection-vehicle-confidence-threshold' '0.6' &
+			vtoolbox set-device-config -u $auth -d $device -p 'camera-rotate' '180' &
+			vtoolbox set-device-config -u $auth -d $device -p 'rtsp.max-viewers' '2' &
+			vtoolbox set-device-config -u $auth -d $device -p 'release-info.channel' 'customer-stable' &
+			vtoolbox set-device-config -u $auth -d $device -p 'stats-monitor-config.system-stats-window' '180' &
+			vtoolbox set-device-config -u $auth -d $device -p 'storage-manage.cloud-backup-location' 'b2' &
+			vtoolbox set-device-config -u $auth -d $device -p 'stream-configs.high.stream-encoding' 'HEVC' &
+			vtoolbox set-device-config -u $auth -d $device -p 'low.stream-encoding' 'HEVC' &
+			vtoolbox set-device-config -u $auth -d $device -p 'cloud-backup-presigned-timeout' '60' &
+			vtoolbox set-device-config -u $auth -d $device -p 'record-on-motion-expansion' '5' &
+			vtoolbox set-device-config -u $auth -d $device -p 'thumbnail-repeat-interval' '300' &
+			vtoolbox set-device-config -u $auth -d $device -p 'vda.motion-notification-divisor' '4' &
+			vtoolbox set-device-config -u $auth -d $device -p 'vstream.high.enabled' 'false' &
+			# The assumption that the last thread will have the same runtime as the previous threads
+			LT=${!} # Get pid of the last thread
+			wait ${LT} # Wait for the threaad to finish
 			echo "End of camera reset for camera $device."
 			# End of camera config (dome default)
 		done
