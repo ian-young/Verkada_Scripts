c1ede514761f2eccda414669c94adf518d34a56a
Ian Young <ianyoung@verkada.com>
2023-09-01 09:53:15 -0600

    Cleaned the output for the update script.

--- Fish-Funcs/update.fish
+++ Fish-Funcs/update.fish
@@ -1,3 +1,6 @@
 function update --description 'Updates all applications installed via terminal'
-	brew update;brew upgrade;pip3 install --upgrade verkada-vtoolbox;python3 -m pip install --upgrade pip
+	brew update
+	brew upgrade
+	pip3 install --upgrade verkada-vtoolbox >> /dev/null
+	python3 -m pip install --upgrade pip >> /dev/null
 end
